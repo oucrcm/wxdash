72c504ddda9e62aeee70fdb6077dd3a7e4092157
ripberjt <jtr@ou.edu>
2022-09-19 14:09:14 -0500

    Optimize

---
+++ 99_calculate_state_predictions.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(robustbase)
+library(sf)
+library(rmapshaper)
+
+downloads <- "/Users/josephripberger/Dropbox (Univ. of Oklahoma)/Severe Weather and Society Dashboard/local files/downloads/" # define locally!!!
+outputs <- "/Users/josephripberger/Dropbox (Univ. of Oklahoma)/Severe Weather and Society Dashboard/local files/outputs/" # define locally!!!
+
+# Make Shapefile ----------------------------------------
+state_shp <- read_sf(paste0(downloads, "s_22mr22"), "s_22mr22")
+
+# Write Shapefile ----------------------------------------
+state_shp <- ms_simplify(state_shp, keep = 0.05)
+st_write(state_shp, paste0(outputs, "state_estimates"), "state_estimates", driver = "ESRI Shapefile", append = FALSE)

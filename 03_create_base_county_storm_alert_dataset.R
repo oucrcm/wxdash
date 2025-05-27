6aad3638a1bbfadde608518a300ed221f8cefa0f
ripberjt <jtr@ou.edu>
2025-05-27 12:09:49 -0500

    Update 03_create_base_county_storm_alert_dataset.R

--- 03_create_base_county_storm_alert_dataset.R
+++ 03_create_base_county_storm_alert_dataset.R
@@ -9,6 +9,8 @@ outputs <- "/Users/josephripberger/Dropbox (Univ. of Oklahoma)/Severe Weather an
 
 # Import Shapefiles -----------------------------
 wwa_paths <- list.files(downloads, full.names = TRUE, pattern = "_all")[1:2] # source: https://mesonet.agron.iastate.edu/request/gis/watchwarn.phtml 
+cnty_shp <- st_read(paste0(downloads, "cb_2023_us_county_20m")) |>
+  st_transform(crs = 5070)
 
 # Function to Process Shapefiles -----------------
 process_wwa_year <- function(shp_path) {

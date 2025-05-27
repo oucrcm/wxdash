d3dff4a734abf2eda8785eb424502802cb2d32fe
ripberjt <jtr@ou.edu>
2025-05-27 11:05:06 -0500

    Update file structure

--- 01_create_county_to_cwa_dataset.R
+++ 01_create_county_to_cwa_dataset.R
@@ -4,8 +4,7 @@ library(sf)
 downloads <- "/Users/josephripberger/Dropbox (Univ. of Oklahoma)/Severe Weather and Society Dashboard/local files/downloads/" # define locally!!!
 outputs <- "/Users/josephripberger/Dropbox (Univ. of Oklahoma)/Severe Weather and Society Dashboard/local files/outputs/" # define locally!!!
 
-# shapefiles -----------------
-cwa_shp <- st_read("~/Dropbox (Univ. of Oklahoma)/Severe Weather and Society Dashboard/local files/downloads/w_18mr25/") |> st_transform(crs = 5070)
+# Import Shapefiles -----------------
 cnty_shp <- st_read("~/Univ. of Oklahoma Dropbox/Joe Ripberger/nws_product_climatology/wwa_data/wwa_shiny_app/data/cb_2023_us_county_500k") |> st_transform(crs = 5070)
 cwa_cnty_shp <- st_read("~/Dropbox (Univ. of Oklahoma)/Severe Weather and Society Dashboard/local files/downloads/c_18mr25") |> st_transform(crs = 5070)
 
@@ -15,8 +14,8 @@ cwa_cnty_data <- cwa_cnty_shp |>
   mutate(CWA = ifelse(FIPS == "12087", "KEY", CWA)) |> # assign KEY to MFL (Miami) for counties that span multiple CWAs
   distinct(FIPS, CWA, .keep_all = TRUE) # remove duplicate FIPS codes (counties that span multiple CWAs)
 
-cwa_cnty_data |> filter(!FIPS %in% cnty_shp$GEOID) |> tibble() |> print(n = Inf) # CT counties, PW, MH, FM
-cnty_shp |> filter(!GEOID %in% cwa_cnty_data$FIPS) |> print(n = Inf) # CT Planning Regions, HI Kalawao County not in cwa_cnty_shp
+cwa_cnty_data |> filter(!FIPS %in% cnty_shp$GEOID) |> tibble() |> print(n = Inf) # n = 45 (CT, PW, MH, FM)
+cnty_shp |> filter(!GEOID %in% cwa_cnty_data$FIPS) |> print(n = Inf) # n = 10 (CT Planning Regions, HI Kalawao County) # fix below
 
 cnty_shp <- cnty_shp |> 
   left_join(cwa_cnty_data |> select(FIPS, CWA), by = c("GEOID" = "FIPS")) |> 
@@ -34,6 +33,7 @@ cnty_shp <- cnty_shp |>
     TRUE ~ CWA
   ))
 
-
+# Write Data -----------------------------
+write_csv(cnty_shp |> st_drop_geometry(), paste0(outputs, "county_to_cwa_data.csv"))
 
 
